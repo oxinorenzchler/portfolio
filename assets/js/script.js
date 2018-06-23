b7caccb7ae617d5787d0281b24b3e13768d40b30
oxinorenzchler <oxino.renzchler@gmail.com>
2018-06-23 10:50:28 -0700

    Added Styles

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	console.log('kjdhskad');
+
+	$('#main-wrapper').fullpage({
+		scrollingSpeed:700,
+		sectionsColor:['orange','#000','#28023D','gray','lavender'],
+		scrollBar:false,
+		navigation:true,
+		slidesNavigation:true,
+		controlArrows:false,
+
+	});
+
+});
---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
+	<title>Portfolio | Renzchler Oxiño</title>
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Raleway:Roboto+Condensed:700" rel="stylesheet">
+	<!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="assets/css/mdb.min.css" rel="stylesheet">
+	<!-- fullPage.js CSS -->
+	<link rel="stylesheet" type="text/css" href="assets/fullpagejs/jquery.fullPage.css">
+	<!-- Custom Stylesheet -->
+	<link rel="stylesheet" type="text/css" href="assets/css/styles.css">
+	<!-- Jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+</head>
+<body>
+	<!-- Main Content -->
+	<div id="main-wrapper"> 
+		<div id="intro" class="section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6">
+						<button class="btn btn-sm btn-default">Download CV</button>
+						<div class="w-100 animated wobble">
+							<img src="assets/img/astro.png" class="img-responsive" alt="astronaut" id="astro">
+						</div>
+					</div>
+					<div class="col-lg-6 white-text animated fadeIn" id="intro-right">
+						<span class="size-1">Greetings</span> my fellow earth men! I'm on a mission to create something <span class="size-2">Awesome</span>. You have found the one. Let's work <span class="size-2">Together.</span>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="works" class="section  animated fadeIn white-text">
+				<h2>Portfolio</h2>
+				<p class="upper grey-text condensed bold">My work</p>
+				<div class="slide active">
+					<div class="container" id="portfolio-container">
+					<div class="row">
+						<div class="col-lg-8" style="">
+							<a>
+								<img src="assets/img/csp1-snapshots.jpg" class="img-responsive w-100 img-thumbnail" alt="Cove Manor Resort &amp; Spa">
+							</a>
+						</div>
+						<div class="col-lg-4">
+							<a href=""  class="capstone-title">Cove Manor Resort &amp; Spa</a>
+							<p class="hide-mobile">
+								Cove Manor Resort &amp; Spa, a world class resort, a paradise that offers unforgettable experience. I am inspired of how beautiful our nature is and at the same time to promote tourism in our country.
+							</p>
+							<p class="hide-mobile">
+								Techonology: HTML5, CSS3, Bootstrap 4, MDBootstrap, JavaScript &amp; Google API
+							</p>
+							<button class="btn btn-default btn-sm show-desktop hide-desktop">Read more</button>
+						</div>					
+					</div>
+					</div>
+				</div>
+			<div class="slide" >ipsum</div>
+			<div class="slide" >dolor</div>
+			<div class="slide" >sit amet</div>
+		</div>
+		<div id="education" class="section">
+			<h2>Education</h2>
+		</div>
+		<div id="skills" class="section">
+			<h2>Skills</h2>
+		</div>
+		<div id="contact" class="section">
+			<h2>Contact</h2>
+		</div>
+	</div>
+
+	<!-- .Main Content -->
+
+
+	<!-- Scripts -->
+	 <script type="text/javascript" src="assets/js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="assets/js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="assets/js/mdb.min.js"></script>
+	<script type="text/javascript" src="assets/fullpagejs/jquery.fullPage.js"></script>
+	<script type="text/javascript" src="assets/js/script.js"></script>
+</body>
+</html>
