5ba1d3c2b69560a5bd586c12cf8a3b26bcc51b47
oxinorenzchler <oxino.renzchler@gmail.com>
2018-06-24 10:08:30 -0700

    Added About and Skills Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,12 @@ $(document).ready(function(){
 
 	$('#main-wrapper').fullpage({
 		scrollingSpeed:700,
-		sectionsColor:['orange','#000','#28023D','#fff','lavender'],
+		sectionsColor:['orange','#000','#28023D','#fff','#FF5F45','lavender'],
 		scrollBar:false,
 		navigation:true,
 		slidesNavigation:true,
 		controlArrows:false,
+		loopHorizontal:true
 
 	});
 
--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE-Edge">
+	<meta name="description" content="Portfolio | Renzchler Oxiño">
+	<meta name="author" content="Renzchler Oxiño">
 	<title>Portfolio | Renzchler Oxiño</title>
 	<!-- Google Fonts -->
 	<link href="https://fonts.googleapis.com/css?family=Poppins|Raleway" rel="stylesheet">
@@ -34,8 +36,33 @@
 						</div>
 					</div>
 					<div class="col-lg-6 white-text animated fadeIn" id="intro-right">
-						<span class="size-1">Greetings</span> my fellow earth men! I'm on a mission to create something <span class="size-2">Awesome</span>. You have found the one. Let's work <span class="size-2">Together.</span>
+						<div id="greetings">
+							<span class="size-1">Greetings</span> my fellow earth men! I'm on a mission to create something <span class="size-2">Awesome</span>. You have found the one. Let's work <span class="size-2">Together.</span>
+						</div>
+						<ul class="d-flex list-unstyled" id="social">
+							<li class="mr-2">
+								<a href="https://www.linkedin.com/in/renzchler-oxi%C3%B1o-0a3aba160/" target="_blank" class="white-text"><i class="fab fa-linkedin"></i>
+								</a>
+							</li>
+							<li class="mr-2">
+								<a href="https://www.instagram.com/zchlerox/" target="_blank" class="white-text"><i class="fab fa-instagram"></i>
+								</a>
+							</li>
+							<li class="mr-2">
+								<a href="https://www.facebook.com/renzch.oxino" target="_blank" class="white-text"><i class="fab fa-facebook-square"></i>
+								</a>
+							</li>
+							<li class="mr-2">
+								<a href="https://twitter.com/iRenzOx" target="_blank" class="white-text"><i class="fab fa-twitter"></i>
+								</a>
+							</li>
+							<li class="mr-2">
+								<a href="https://github.com/oxinorenzchler" target="_blank" class="white-text"><i class="fab fa-github-square"></i>
+								</a>
+							</li>
+						</ul>
 					</div>
+					
 				</div>
 			</div>
 		</div>
@@ -168,7 +195,7 @@
 							<li>Focused on user friendly &amp; mobile first web development.</li>
 							<li>Plan database and relations for websites.</li>
 							<li>Experience in MVC Framework.</li>
-							<li>Deploying website to a live server.</li>
+							<li>Deploying website to web.</li>
 						</ul>
 					</div>
 				</div>
@@ -190,7 +217,7 @@
 					<p class="upper gray-text">Graduated in November 2016 ( 4 years )</p>
 					</div>
 					<div class="mb-2">
-						<h3 class="font-weight-bold">BS Information Technology</h3>
+						<h3 class="font-weight-bold">Full Stack Web Developer</h3>
 					<h5><a href="https://tuitt.com/" target="_blank" class="orange-text">Tuitt Coding Bootcamp</a></h5>
 					<p class="upper gray-text">March 2018 - June 2018 ( 3 months )</p>
 					</div>
@@ -198,42 +225,128 @@
 				</div>
 			</div>
 		</div>
-		<div id="contact" class="section poppins">
-			<div class="container">
+		<div id="skills" class="section poppins white-text animated fadeIn">
+			<div class="container mt-2">
 				<div class="row">
-					<div class="col-lg-6">
+					<div class="col-lg-6 mb-3">
 						<div class="line mb-2"></div>
-						<h2 class="font-weight-bold">About me</h2>
+						<h2 class="font-weight-bold"><i class="fas fa-rocket"></i> Skills</h2>
+						<span class="upper">Expertise</span>
+					</div>
+					<div class="col-lg-6">
+						<div class="mb-1">
+							<h5 class="font-weight-bold">HTML5</h5>
+							<div class="progress">
+						    <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" id="html">
+						      <span class="sr-only">85% HTML</span>
+						    </div>
+						  </div>
+						</div>
+						<div class="mb-1">
+							<h5 class="font-weight-bold">CSS3</h5>
+							<div class="progress">
+						    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" id="css">
+						      <span class="sr-only">80% CSS3</span>
+						    </div>
+						  </div>
+						</div>
+						<div class="mb-1">
+							<h5 class="font-weight-bold">Bootstrap</h5>
+							<div class="progress">
+						    <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" id="bootstrap">
+						      <span class="sr-only">85% Bootstrap</span>
+						    </div>
+						  </div>
+						</div>
+						<div class="mb-1">
+							<h5 class="font-weight-bold">Javascript</h5>
+							<div class="progress">
+						    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" id="javascript">
+						      <span class="sr-only">80% Javascript</span>
+						    </div>
+						  </div>
+						</div>
+						<div class="mb-1">
+							<h5 class="font-weight-bold">MySQL</h5>
+							<div class="progress">
+						    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" id="mysql">
+						      <span class="sr-only">80% MySQL</span>
+						    </div>
+						  </div>
+						</div>
+						<div class="mb-1">
+							<h5 class="font-weight-bold">PHP</h5>
+							<div class="progress">
+						    <div class="progress-bar" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100" id="php">
+						      <span class="sr-only">85% PHP</span>
+						    </div>
+						  </div>
+						</div>
+						<div class="mb-1">
+							<h5 class="font-weight-bold">Laravel</h5>
+							<div class="progress">
+						    <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" id="laravel">
+						      <span class="sr-only">80% Laravel</span>
+						    </div>
+						  </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="contact" class="section poppins">
+			<div class="container mt-2">
+				<div class="row">
+					<div class="col-lg-12">
+						<div class="line-desktop mb-2"></div>
+						<h2 class="font-weight-bold"><i class="fas fa-address-card"></i> About me</h2>
+						<span class="upper">My info</span>
 						<div class="w-100 d-flex mt-3">
-							<div class="row">
+							<div class="row mx-auto">
 								<div class="col-md-6 w-100">
 									<img src="assets/img/profile.jpg" class="img-fluid rounded-circle img-thumbnail" id="profile-avatar">
 								</div>
 								<div class="col-md-6">
 									<ul class="list-unstyled font-weight-bold white-text pull-left" id="contact-info">
-										<li class="mb-2"><i class="fas fa-map-marker-alt mr-2"></i> Quezon City, Philippines</li>
-										<li class="mb-2"><i class="fa fa-phone mr-1" aria-hidden="true"></i> +639270757234</li>
+										<li class="mb-3"><i class="fas fa-map-marker-alt mr-2"></i> Quezon City, Philippines</li>
+										<li class="mb-3"><i class="fa fa-phone mr-1" aria-hidden="true"></i> +639270757234</li>
 										<li class="d-flex"><i class="fas fa-envelope mr-2"></i> <a class="white-text" href="mailto:oxino.renzchler@gmail.com">oxino.renzchler@gmail.com</a></li>
 									</ul>
 								</div>
 							</div>
 						</div>
-						<p class="font-weight-bold">
+						<div class="row">
+						<div class="col-md-2"></div>
+						<div class="col-md-8">
+							<p class="font-weight-bold hide-mobile">
 							I am passionate about everything related to web development. I always want to learn something new. I'm flexible and hardworking. I have experience in creating dynamic and responsive websites. With my skills and perseverance I believe we can achieve the company's goal. Let's create something awesome!
 						</p>
-					</div>
-					<div class="col-lg-6">
-						<div class="line mb-2"></div>
-						<h2 class="font-weight-bold">Skills</h2>
+						</div>
+						<div class="col-md-2"></div></div>
+						<button data-toggle="modal" data-target="#aboutme" class="btn btn-sm btn-default hide-desktop ml-3">Read more</button>
 					</div>
 				</div>
 			</div>
-			
 		</div>
 	</div>
 
 	<!-- .Main Content -->
-
+	<!-- Modal About Me -->
+	<div class="modal fade poppins" id="aboutme" tabindex="-1" role="dialog" aria-labelledby="readmore" aria-hidden="true">
+	    <div class="modal-dialog" role="document">
+	        <div class="modal-content">
+	            <div class="modal-header">
+	                <h5 class="modal-title" id="readmore">What inspire me?</h5>
+	            </div>
+	            <div class="modal-body">
+	            	<p>I am passionate about everything related to web development. I always want to learn something new. I'm flexible and hardworking. I have experience in creating dynamic and responsive websites. With my skills and perseverance I believe we can achieve the company's goal. Let's create something awesome!</p>
+	            </div>
+	            <div class="modal-footer">
+	                <button type="button" class="btn btn-default btn-sm" data-dismiss="modal">Close</button>
+	            </div>
+	        </div>
+	    </div>
+	</div>
 
 	<!-- Modal Slide 1 -->
 	<div class="modal fade poppins" id="slide1" tabindex="-1" role="dialog" aria-labelledby="readmore" aria-hidden="true">
